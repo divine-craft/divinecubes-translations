0e77fd1b7917dc852cc2d00bb550977f21f9a939
CatCoderr <catcoderr@gmail.com>
2021-11-02 23:37:28 +0200

    fix: use multi-stage building

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,24 @@
-FROM nginx:alpine
-
-ARG VERSION=1.0.0
+FROM alpine:latest AS builder
 
 RUN apk add curl --no-cache
 
 RUN curl -L -O https://github.com/CertainLach/jrsonnet/releases/latest/download/jrsonnet-linux-static-amd64
 RUN chmod +x jrsonnet-linux-static-amd64
 
+COPY /translations ./translations
+COPY /vars ./vars
+
 RUN ./vars/build.sh
+
+FROM nginx:alpine
+
+WORKDIR /app
+
+COPY --from=builder ./app .
 
 RUN rm -rf /usr/share/nginx/html/*
 
 COPY ./nginx.conf /etc/nginx/nginx.conf
-
-WORKDIR /app
-
-COPY ./app /app
 
 EXPOSE 8080
 
--- vars/build.sh
+++ vars/build.sh
@@ -4,11 +4,11 @@ for server in divinecraft flexmine julius kanddy village; do
 	mkdir -p ./app/$server/src/lang
 	for lang in en ru uk; do
 		./jrsonnet-linux-static-amd64 \
-			--ext-code=prefixes="import 'vars/prefixes/$lang.jsonnet'" \
-			--ext-code=vars="import 'vars/$server.jsonnet'" \
+			--ext-code=prefixes="import '/vars/prefixes/$lang.jsonnet'" \
+			--ext-code=vars="import '/vars/$server.jsonnet'" \
 			--ext-str=serverName=$server \
 			--ext-str=lang=$lang --ext-str=fallbackLang=ru -f yaml \
-			vars/build.jsonnet > ./app/$server/src/lang/$lang.yml &
+			./vars/build.jsonnet > ./app/$server/src/lang/$lang.yml &
 	done
 done
 
