d02e274ab74188157c5cc03963088e04f657fcb8
CatCoderr <catcoderr@gmail.com>
2021-11-30 12:15:56 +0200

    perf: use alpine linux

--- vars/build.sh
+++ vars/build.sh
@@ -1,24 +1,13 @@
-#!/bin/bash
-
-set -eu
+#!/bin/ash
 
 for server in divinecraft bunnyboom flexmine julius kanddy village; do
 	mkdir -p ./app/$server/src/lang
-	for lang in en ru uk; do
+	for lang in ru; do
 		./jrsonnet-linux-static-amd64 \
 			--ext-code=prefixes="import '/vars/prefixes/$lang.jsonnet'" \
 			--ext-code=vars="import '/vars/$server.jsonnet'" \
 			--ext-str=serverName=$server \
 			--ext-str=lang=$lang --ext-str=fallbackLang=ru -f yaml \
-			./vars/build.jsonnet > ./app/$server/src/lang/$lang.yml &
+			./vars/build.jsonnet > ./app/$server/src/lang/$lang.yml
 	done
 done
-
-# Wait for all jobs
-while true; do
-	wait -n || {
-		code="$?"
-		([[ $code = "127" ]] && exit 0 || exit "$code")
-		break
-	}
-done;
