43409d92a4b74bede72f1a549694fd2f5274835e
CatCoderr <catcoderr@gmail.com>
2021-12-20 17:41:28 +0200

    chore: add english lang to build.sh

--- vars/build.sh
+++ vars/build.sh
@@ -2,7 +2,7 @@
 
 for server in divinecraft bunnyboom flexmine julius kanddy village gribland; do
 	mkdir -p ./app/$server/src/lang
-	for lang in ru; do
+	for lang in en ru; do
 		./jrsonnet-linux-static-amd64 \
 			--ext-code=prefixes="import '/vars/prefixes/$lang.jsonnet'" \
 			--ext-code=vars="import '/vars/$server.jsonnet'" \
